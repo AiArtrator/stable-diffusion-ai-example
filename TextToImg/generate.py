2085fb6aee9f48c2656c305b161619e42a2e4979
doheeMac <thsehgml37@korea.ac.kr>
2022-11-28 20:44:03 +0900

    feat: 4. Set up initial generation parameters, display image on generation, and warning for if the safety filter is tripped...

---
+++ TextToImg/generate.py
@@ -0,0 +1,29 @@
+# Set up our initial generation parameters.
+answers = stability_api.generate(
+    prompt="expansive landscape rolling greens with blue daisies and weeping willow trees under a blue alien sky, artstation, masterful, ghibli",
+    seed=992446758, # If a seed is provided, the resulting generated image will be deterministic.
+                    # What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
+                    # Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
+    steps=50, # Step Count defaults to 50 if not specified here.
+    cfg_scale=8.0, # Influences how strongly your generation is guided to match your prompt.
+                   # Setting this value higher increases the strength in which it tries to match your prompt.
+                   # Defaults to 7.0 if not specified.
+    width=512, # Generation width, defaults to 512 if not included.
+    height=512, # Generation height, defaults to 512 if not included.
+    samples=1, # Number of images to generate, defaults to 1 if not included.
+    sampler=generation.SAMPLER_K_DPM_2_ANCESTRAL # Choose which sampler we want to denoise our generation with.
+                                                 # Defaults to k_lms if not specified. Clip Guidance only supports ancestral samplers.
+                                                 # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m)
+)
+
+# Set up our warning to print to the console if the adult content classifier is tripped.
+# If adult content classifier is not tripped, save generated images.
+for resp in answers:
+    for artifact in resp.artifacts:
+        if artifact.finish_reason == generation.FILTER:
+            warnings.warn(
+                "Your request activated the API's safety filters and could not be processed."
+                "Please modify the prompt and try again.")
+        if artifact.type == generation.ARTIFACT_IMAGE:
+            img = Image.open(io.BytesIO(artifact.binary))
+            img.save(str(artifact.seed)+ ".png") # Save our generated images with their seed number as the filename.
