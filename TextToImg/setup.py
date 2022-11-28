d4848b6e9d497ba9c3b4ece3b82eac4bc2007dd3
doheeMac <thsehgml37@korea.ac.kr>
2022-11-28 20:36:29 +0900

    feat: text to image - import dependencies and set up environmnet variables and API key

---
+++ TextToImg/setup.py
@@ -0,0 +1,22 @@
+# pip install stability-sdk
+# 2. Import dependencies and set up our environment variables and API Key.
+
+import os
+import io
+import warnings
+from PIL import Image
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+
+# Our Host URL should not be prepended with "https" nor should it have a trailing slash.
+os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
+
+# Sign up for an account at the following link to get an API Key.
+# https://beta.dreamstudio.ai/membership
+
+# Click on the following link once you have created an account to be taken to your API Key.
+# https://beta.dreamstudio.ai/membership?tab=apiKeys
+
+# Paste your API Key below.
+
+os.environ['STABILITY_KEY'] = 'key-goes-here'
